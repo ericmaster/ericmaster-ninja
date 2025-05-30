a92776f1db6d25800ac635005f6fb378d6ccc8a5
Eric Aguayo <ericmaster@nimblersoft.com>
2025-05-30 15:50:47 +0000

    Updates

--- worker/index.ts
+++ worker/index.ts
@@ -1,51 +1,44 @@
-export default {
-  async fetch(
-    request: Request,
-    env: unknown,
-    // ctx: ExecutionContext
-  ): Promise<Response> {
-    const { GH_CLIENT_ID: client_id, GH_CLIENT_SECRET: client_secret } = env as { GH_CLIENT_ID: string; GH_CLIENT_SECRET: string; };
-    const url = new URL(request.url);
-    if (!url.pathname.includes("/api/auth")) {
-      return new Response(null, { status: 404 });
-    }
+const handleAuth = async (
+  url: URL,
+  env: unknown
+): Promise<Response> => {
+  const { GH_CLIENT_ID: client_id, GH_CLIENT_SECRET: client_secret } = env as {
+    GH_CLIENT_ID: string;
+    GH_CLIENT_SECRET: string;
+  };
+  const code = url.searchParams.get("code");
 
-    const code = url.searchParams.get("code");
+  if (!code) {
+    const params = new URLSearchParams({
+      client_id: client_id,
+      redirect_uri: "https://ericmaster.ninja/api/auth",
+      scope: "read:user user:email repo",
+    });
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?${params.toString()}`;
+    return Response.redirect(githubAuthUrl);
+  }
 
-    if (!code) {
-      const params = new URLSearchParams({
-        client_id: client_id,
-        redirect_uri: "https://ericmaster.ninja/api/auth",
-        scope: "read:user user:email repo",
-      });
-      const githubAuthUrl = `https://github.com/login/oauth/authorize?${params.toString()}`;
-      return Response.redirect(githubAuthUrl);
-    }
+  // Exchange code for access token
+  const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      client_id,
+      client_secret,
+      code,
+      redirect_uri: "https://ericmaster.ninja/api/auth",
+    }),
+  });
+  const tokenData = (await tokenRes.json()) as { access_token?: string };
+  if (!tokenData.access_token) {
+    return new Response("OAuth failed", { status: 401 });
+  }
 
-    // Exchange code for access token
-    const tokenRes = await fetch(
-      "https://github.com/login/oauth/access_token",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          client_id,
-          client_secret,
-          code,
-          redirect_uri: "https://ericmaster.ninja/api/auth",
-        }),
-      }
-    );
-    const tokenData = (await tokenRes.json()) as { access_token?: string };
-    if (!tokenData.access_token) {
-      return new Response("OAuth failed", { status: 401 });
-    }
-
-    const content = { token: tokenData.access_token, provider: "github" };
-    const html = `<!DOCTYPE html>
+  const content = { token: tokenData.access_token, provider: "github" };
+  const html = `<!DOCTYPE html>
 <html>
   <body>
     <script>
@@ -68,8 +61,25 @@ export default {
     </script>
   </body>
 </html>`;
-    return new Response(html, {
-      headers: { "Content-Type": "text/html" },
-    });
-  }
+  return new Response(html, {
+    headers: { "Content-Type": "text/html" },
+  });
+};
+
+export default {
+  async fetch(
+    request: Request,
+    env: unknown
+    // ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    switch (url.pathname) {
+      case "/ai-cheatsheets":
+        return fetch('https://ericmaster.github.io' + url.pathname);
+      case "/api/auth":
+        return handleAuth(url, env);
+      default:
+        return new Response("Not Found", { status: 404 });
+    }
+  },
 } satisfies ExportedHandler;
